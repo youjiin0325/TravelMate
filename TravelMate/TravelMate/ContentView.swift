cf1141ef9c45e321a13878c270ade818ddb1e733
youjiin0325 <youjiin0325@gmail.com>
2024-04-17 23:45:20 +0900

    feat: 파일 생성

---
+++ TravelMate/TravelMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TravelMate
+//
+//  Created by 유지인 on 4/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelMate/TravelMate/TravelMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelMateApp.swift
+//  TravelMate
+//
+//  Created by 유지인 on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
